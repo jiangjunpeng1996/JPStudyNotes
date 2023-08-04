c2af8b62c5b2c2a7ba7de1746563f91b81dc5c5b
junpeng.jiang <junpeng.jiang1996@gmail.com>
2023-08-04 16:01:03 +0800

    feat: 项目中element-plus集成

--- Vue3/vue3_admin/src/App.vue
+++ Vue3/vue3_admin/src/App.vue
@@ -1,8 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 引入图标组件
+import { Plus, Edit, Delete } from '@element-plus/icons-vue'
+</script>
 
 <template>
   <div>
-    <h1>我是App</h1>
+    <el-button type="primary" size="default" :icon="Plus">Primary</el-button>
+    <el-button type="success" size="default" :icon="Edit">Edit</el-button>
+    <el-button type="danger" size="default" :icon="Delete">Delete</el-button>
+    <el-pagination
+      :total="400"
+      :page-size="100"
+      :page-sizes="[10, 25, 50, 100]"
+      layout="total, sizes, prev, pager, next, jumper"
+    />
   </div>
 </template>
 
--- Vue3/vue3_admin/src/main.ts
+++ Vue3/vue3_admin/src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 引入element-plus插件与样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 配置element-plus国际化
+
+// 忽略当前文件ts类型的检测，否则有红色提示（打包会失败）
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// 获取应用实例对象
+const app = createApp(App)
+// 安装element-plus插件
+app.use(ElementPlus, {
+  locale: zhCn, // element-plus国际化配置
+})
+// 将应用挂载到挂载点上
+app.mount('#app')
