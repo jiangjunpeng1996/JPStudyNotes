9c914c0c68010f9ddb70db0ca70a415156ea2042
junpeng.jiang <junpeng.jiang1996@gmail.com>
2023-08-04 15:11:01 +0800

    feat: 项目中stylelint工具配置

--- Vue3/vue3_admin/src/App.vue
+++ Vue3/vue3_admin/src/App.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
@@ -8,5 +6,5 @@
   </div>
 </template>
 
-<style scoped>
+<style lang="scss">
 </style>
