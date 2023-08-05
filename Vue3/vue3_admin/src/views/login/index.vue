6c832913b2ae879b9bf306c03938115499720076
junpeng.jiang <junpeng.jiang1996@gmail.com>
2023-08-05 15:30:39 +0800

    feat: 登录路由静态页面搭建

--- Vue3/vue3_admin/src/views/login/index.vue
+++ Vue3/vue3_admin/src/views/login/index.vue
@@ -1,9 +1,69 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { User, Lock } from '@element-plus/icons-vue'
+import { reactive } from 'vue'
+// 收集账号与密码的数据
+let loginForm = reactive({
+  username: 'admin',
+  password: '123456',
+})
+</script>
 
 <template>
-  <div>
-    <h1>login</h1>
+  <div class="login_container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>欢迎来到后台管理系统</h2>
+          <el-form-item>
+            <el-input
+              :prefix-icon="User"
+              v-model="loginForm.username"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="password"
+              :prefix-icon="Lock"
+              show-password
+              v-model="loginForm.password"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login_btn" type="primary" size="default">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.login_container {
+  width: 100%;
+  height: 100vh;
+  background: url('@/assets/images/background.jpg') no-repeat;
+  background-size: 100% 100%;
+  .login_form {
+    position: relative;
+    width: 80%;
+    top: 30vh;
+    background: url('@/assets/images/login_form.png') no-repeat;
+    background-size: cover;
+    padding: 40px;
+    h1 {
+      color: #fff;
+      font-size: 40px;
+    }
+    h2 {
+      color: #fff;
+      font-size: 20px;
+      margin: 20px 0;
+    }
+    .login_btn {
+      width: 100%;
+    }
+  }
+}
+</style>
