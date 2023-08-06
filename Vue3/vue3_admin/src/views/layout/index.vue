41e87a4c041853c9ea4c1a77d36753be938b483b
junpeng.jiang <junpeng.jiang1996@gmail.com>
2023-08-06 14:28:47 +0800

    feat: 左侧菜单静态页面搭建

--- Vue3/vue3_admin/src/views/layout/index.vue
+++ Vue3/vue3_admin/src/views/layout/index.vue
@@ -8,6 +8,23 @@ import Logo from './logo/index.vue'
     <!-- 左侧菜单 -->
     <div class="layout_sidebar">
       <Logo></Logo>
+      <!-- 展示菜单 -->
+      <!-- 滚动组件 -->
+      <el-scrollbar class="scrollbar">
+        <!-- 菜单组件 -->
+        <el-menu background-color="#001529" text-color="#fff">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">数据大屏</el-menu-item>
+          <el-sub-menu index="3">
+            <template #title>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="3-1">用户管理</el-menu-item>
+            <el-menu-item index="3-2">角色管理</el-menu-item>
+            <el-menu-item index="3-3">菜单管理</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!-- 顶部导航 -->
     <div class="layout_tabbar">2</div>
@@ -26,6 +43,10 @@ import Logo from './logo/index.vue'
     width: $base-menu-width;
     height: 100vh;
     background: $base-menu-background;
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-menu-logo-height);
+    }
   }
   .layout_tabbar {
     width: calc(100% - $base-menu-width);
