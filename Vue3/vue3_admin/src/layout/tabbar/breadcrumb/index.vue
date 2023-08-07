43820c4fd92d06241af950e41325c7d90335616e
junpeng.jiang <junpeng.jiang1996@gmail.com>
2023-08-07 16:41:22 +0800

    feat: 顶部面包屑动态展示

--- Vue3/vue3_admin/src/layout/tabbar/breadcrumb/index.vue
+++ Vue3/vue3_admin/src/layout/tabbar/breadcrumb/index.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import useLayoutSettingStore from '@/store/modules/setting'
+import { useRoute } from 'vue-router'
 // 获取layout配置相关仓库
 let layoutSettingStore = useLayoutSettingStore()
 // 点击图标的方法
@@ -7,6 +8,8 @@ const changeIcon = () => {
   // 图标切换
   layoutSettingStore.fold = !layoutSettingStore.fold
 }
+// 获取路由对象
+let $route = useRoute()
 </script>
 
 <script lang="ts">
@@ -21,8 +24,20 @@ export default {
   </el-icon>
   <!-- 面包屑 -->
   <el-breadcrumb separator-icon="ArrowRight">
-    <el-breadcrumb-item>权限管理</el-breadcrumb-item>
-    <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+    <!-- 动态展示路由名字与图标 -->
+    <el-breadcrumb-item
+      v-for="item in $route.matched"
+      :key="item.path"
+      v-show="item.meta.title"
+      :to="item.path"
+    >
+      <!-- 图标 -->
+      <el-icon style="margin: 0 2px">
+        <component :is="item.meta.icon"></component>
+      </el-icon>
+      <!-- 面包屑展示匹配路由的标题 -->
+      <span>{{ item.meta.title }}</span>
+    </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
--- Vue3/vue3_admin/src/router/routes.ts
+++ Vue3/vue3_admin/src/router/routes.ts
@@ -17,9 +17,9 @@ export const constantRoute = [
     component: () => import('@/layout/index.vue'),
     name: 'layout',
     meta: {
-      title: 'layout',
+      title: '',
       hidden: false,
-      icon: 'Avatar',
+      icon: '',
     },
     redirect: '/home',
     children: [
@@ -53,6 +53,7 @@ export const constantRoute = [
       title: '权限管理',
       icon: 'Lock',
     },
+    redirect: '/acl/user',
     children: [
       {
         path: '/acl/user',
@@ -95,6 +96,7 @@ export const constantRoute = [
       title: '商品管理',
       icon: 'Goods',
     },
+    redirect: '/product/trademark',
     children: [
       {
         path: '/product/trademark',
