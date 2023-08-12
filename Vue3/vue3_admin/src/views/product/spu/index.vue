d9fc3ae6c863324d12f11709f37382f8c9f20283
junpeng.jiang <junpeng.jiang1996@gmail.com>
2023-08-12 10:21:49 +0800

    feat: SPU模块静态页面搭建

--- Vue3/vue3_admin/src/views/product/spu/index.vue
+++ Vue3/vue3_admin/src/views/product/spu/index.vue
@@ -1,8 +1,39 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+// 场景的数据
+let scene = ref<number>(0)
+// 分页器默认页码
+let pageNo = ref<number>(1)
+// 每一页展示几条数据
+let pageSize = ref<number>(3)
+</script>
 
 <template>
   <div>
-    <h1>SPU管理</h1>
+    <!-- 三级分类 -->
+    <Category :scene="scene" />
+    <el-card style="margin: 10px 0">
+      <el-button type="primary" size="default" icon="Plus">添加SPU</el-button>
+      <el-table border style="margin: 10px 0">
+        <el-table-column
+          label="序号"
+          align="center"
+          type="index"
+          width="80px"
+        ></el-table-column>
+        <el-table-column label="SPU名称"></el-table-column>
+        <el-table-column label="SPU描述"></el-table-column>
+        <el-table-column label="SPU操作"></el-table-column>
+      </el-table>
+      <el-pagination
+        v-model:current-page="pageNo"
+        v-model:page-size="pageSize"
+        :page-sizes="[3, 5, 7, 9]"
+        :background="true"
+        layout="prev, pager, next, jumper, ->, sizes, total"
+        :total="400"
+      />
+    </el-card>
   </div>
 </template>
 
