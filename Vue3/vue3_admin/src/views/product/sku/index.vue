bee5a761143fd9ad668aa377cf3ef154f9b48f05
junpeng.jiang <junpeng.jiang1996@gmail.com>
2023-08-14 10:31:16 +0800

    feat: SKU管理模块静态页面的搭建

--- Vue3/vue3_admin/src/views/product/sku/index.vue
+++ Vue3/vue3_admin/src/views/product/sku/index.vue
@@ -1,9 +1,48 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+// 分页器当前页码
+let pageNo = ref<number>(1)
+// 每一页展示几条数据
+let pageSize = ref<number>(10)
+</script>
 
 <template>
-  <div>
-    <h1>SKU管理</h1>
-  </div>
+  <el-card>
+    <el-table border style="margin: 10px 0">
+      <el-table-column
+        label="序号"
+        type="index"
+        align="center"
+        width="80px"
+      ></el-table-column>
+      <el-table-column
+        label="名称"
+        show-overflow-tooltip
+        width="150px"
+      ></el-table-column>
+      <el-table-column
+        label="描述"
+        show-overflow-tooltip
+        width="150px"
+      ></el-table-column>
+      <el-table-column label="图片" width="150px"></el-table-column>
+      <el-table-column label="重量" width="150px"></el-table-column>
+      <el-table-column label="价格" width="150px"></el-table-column>
+      <el-table-column
+        label="操作"
+        width="250px"
+        fixed="right"
+      ></el-table-column>
+    </el-table>
+    <el-pagination
+      v-model:current-page="pageNo"
+      v-model:page-size="pageSize"
+      :page-sizes="[10, 20, 30, 40]"
+      :background="true"
+      layout="prev, pager, next, jumper,->,sizes, total,"
+      :total="100"
+    />
+  </el-card>
 </template>
 
 <style scoped lang="scss"></style>
