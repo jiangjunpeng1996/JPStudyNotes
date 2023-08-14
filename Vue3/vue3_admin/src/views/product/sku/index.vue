d0b1d2388b684c63d7a9c432b8db4190f27a5f51
junpeng.jiang <junpeng.jiang1996@gmail.com>
2023-08-14 14:20:02 +0800

    feat: 商品详情静态页面搭建

--- Vue3/vue3_admin/src/views/product/sku/index.vue
+++ Vue3/vue3_admin/src/views/product/sku/index.vue
@@ -9,6 +9,8 @@ let pageNo = ref<number>(1)
 let pageSize = ref<number>(10)
 let total = ref<number>(0)
 let skuArr = ref<SkuData[]>([])
+// 控制抽屉显示与隐藏
+let drawer = ref<boolean>(false)
 // 组件挂载完毕
 onMounted(() => {
   getHasSku()
@@ -50,6 +52,12 @@ const updateSale = async (row: SkuData) => {
 // 更新已有的SKU
 const updateSku = () => {
   ElMessage({ type: 'success', message: '程序员在努力的更新中...' })
+}
+
+// 查看商品详情按钮的回调
+const findSku = async (row: SkuData) => {
+  // 显示抽屉
+  drawer.value = true
 }
 </script>
 
@@ -107,7 +115,12 @@ const updateSku = () => {
             icon="Edit"
             @click="updateSku"
           ></el-button>
-          <el-button type="primary" size="small" icon="InfoFilled"></el-button>
+          <el-button
+            type="primary"
+            size="small"
+            icon="InfoFilled"
+            @click="findSku(row)"
+          ></el-button>
           <el-button type="primary" size="small" icon="Delete"></el-button>
         </template>
       </el-table-column>
@@ -122,7 +135,69 @@ const updateSku = () => {
       @current-change="getHasSku"
       @size-change="handler"
     />
+    <!-- 抽屉组件 -->
+    <el-drawer v-model="drawer">
+      <template #header>
+        <h4>查看商品的详情</h4>
+      </template>
+      <template #default>
+        <el-row style="margin: 10px 0">
+          <el-col :span="6">名称</el-col>
+          <el-col :span="18">456</el-col>
+        </el-row>
+        <el-row style="margin: 10px 0">
+          <el-col :span="6">描述</el-col>
+          <el-col :span="18">456</el-col>
+        </el-row>
+        <el-row style="margin: 10px 0">
+          <el-col :span="6">价格</el-col>
+          <el-col :span="18">5000</el-col>
+        </el-row>
+        <el-row style="margin: 10px 0">
+          <el-col :span="6">平台属性</el-col>
+          <el-col :span="18">
+            <el-tag style="margin: 5px" v-for="item in 10" :key="item">
+              {{ item }}
+            </el-tag>
+          </el-col>
+        </el-row>
+        <el-row style="margin: 10px 0">
+          <el-col :span="6">销售属性</el-col>
+          <el-col :span="18">
+            <el-tag style="margin: 5px" v-for="item in 10" :key="item">
+              {{ item }}
+            </el-tag>
+          </el-col>
+        </el-row>
+        <el-row style="margin: 10px 0">
+          <el-col :span="6">商品图片</el-col>
+          <el-col :span="18">
+            <el-carousel :interval="4000" type="card" height="200px">
+              <el-carousel-item v-for="item in 6" :key="item">
+                <h3 text="2xl" justify="center">{{ item }}</h3>
+              </el-carousel-item>
+            </el-carousel>
+          </el-col>
+        </el-row>
+      </template>
+    </el-drawer>
   </el-card>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
