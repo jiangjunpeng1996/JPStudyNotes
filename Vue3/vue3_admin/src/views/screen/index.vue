dfdc89b026050f274e1ba5b04e57e6027c0345c3
junpeng.jiang <junpeng.jiang1996@gmail.com>
2023-08-17 18:06:49 +0800

    feat: 数据大屏顶部的搭建

--- Vue3/vue3_admin/src/views/screen/index.vue
+++ Vue3/vue3_admin/src/views/screen/index.vue
@@ -1,9 +1,69 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+// 引入顶部的子组件
+import Top from './components/top/index.vue'
+import { ref, onMounted } from 'vue'
+// 获取数据大屏展示内容盒子的DOM元素
+let screen = ref()
+onMounted(() => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+})
+// 定义大屏缩放的比例
+const getScale = (w = 1920, h = 1080) => {
+  const ww = window.innerWidth / w
+  const wh = window.innerHeight / h
+  return ww < wh ? ww : wh
+}
+// 监听视口的变化
+window.onresize = () => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+}
+</script>
 
 <template>
-  <div>
-    <h1>数据大屏</h1>
+  <div class="container">
+    <!-- 数据大屏展示内容区域 -->
+    <div class="screen" ref="screen">
+      <div class="top">
+        <Top />
+      </div>
+      <div class="bottom">
+        <div class="left">左</div>
+        <div class="center">中</div>
+        <div class="right">右</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url(./images/bg.png) no-repeat;
+  background-size: cover;
+  .screen {
+    position: fixed;
+    width: 1920px;
+    height: 1080px;
+    left: 50%;
+    top: 50%;
+    transform-origin: left top;
+    .top {
+      width: 100%;
+      height: 40px;
+    }
+    .bottom {
+      display: flex;
+      .right {
+        flex: 1;
+      }
+      .left {
+        flex: 1;
+      }
+      .center {
+        flex: 2;
+      }
+    }
+  }
+}
+</style>
