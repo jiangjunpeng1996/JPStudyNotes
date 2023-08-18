af277c52c91d3227c8cc6c78c0a0a4933b22bd08
junpeng.jiang <junpeng.jiang1996@gmail.com>
2023-08-18 09:31:58 +0800

    feat: 水球图完成

--- Vue3/vue3_admin/src/views/screen/components/tourist/index.vue
+++ Vue3/vue3_admin/src/views/screen/components/tourist/index.vue
@@ -1,6 +1,54 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+// 引入水球图拓展插件
+import 'echarts-liquidfill'
+import * as Echarts from 'echarts'
 let people = ref('216908人')
+
+// 获取节点
+let charts = ref()
+onMounted(() => {
+  // 获取echarts类的实例
+  let myCharts = Echarts.init(charts.value)
+  // 设置实例的配置项
+  myCharts.setOption({
+    // 标题组件
+    title: {
+      text: '水球图',
+    },
+    // x|y轴组件
+    xAxis: {},
+    yAxis: {},
+    // 系列: 决定你展示什么样的图形图标
+    series: {
+      type: 'liquidFill', // 系列
+      data: [0.6, 0.4, 0.2], // 展示的数据
+      waveAnimation: true, // 动画
+      animationDuration: 3,
+      animationDurationUpdate: 0,
+      radius: '90%', // 半径
+      outline: {
+        // 外层边框颜色设置
+        show: true,
+        borderDistance: 8,
+        itemStyle: {
+          color: 'skyblue',
+          borderColor: '#294D99',
+          borderWidth: 8,
+          shadowBlur: 20,
+          shadowColor: 'rgba(0, 0, 0, 0.25)',
+        },
+      },
+    },
+    // 布局组件
+    grid: {
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+    },
+  })
+})
 </script>
 
 <template>
@@ -17,7 +65,7 @@ let people = ref('216908人')
     <div class="number">
       <span v-for="(item, index) in people" :key="index">{{ item }}</span>
     </div>
-    <div class="charts" ref="charts">123</div>
+    <div class="charts" ref="charts"></div>
   </div>
 </template>
 
